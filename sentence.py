81c2204626478055cf437c74702e5fb87a404022
rronakk <rray@mediamath.com>
2018-03-08 17:31:18 -0500

    removing unnecessary comments

--- sentence.py
+++ sentence.py
@@ -1,9 +1,3 @@
-# In the programming language of your choice create a class with a method to return the length and longest words in a sentence. For example, “The cow jumped over the moon.” should return 6 and “jumped”.
-# Create unit tests to test that method, reworking your code if needed.
-# Explain any assumptions in comments.
-# Add a README explaining how to execute your tests.
-# Share via GitHub etc or email.
-
 import re
 
 
